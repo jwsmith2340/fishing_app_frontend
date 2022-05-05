ba43eda4569f9e150f0bc082df5d4e0589e21929
jwsmith2340 <92739468+jwsmith2340@users.noreply.github.com>
2022-05-05 19:12:15 -0400

    Added Rivers page, not yet populated with anything.

---
+++ src/pages/Rivers.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+function Rivers(props) {
+    return (
+        <>
+            <h1>Rivers page</h1>
+        </>
+    )
+}
+
+export default Rivers;

2b1c9ddd8d61282d0bebd3dbfa19c0aca81bc89f
jwsmith2340 <92739468+jwsmith2340@users.noreply.github.com>
2022-05-21 12:54:54 -0400

    setStates removed and placed in indeces.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,30 +2,20 @@ import {useState, useEffect, useRef} from 'react'
 import {Route, Switch, Redirect} from 'react-router-dom'
 
 import Index from '../pages/Index'
+import Flies from '../pages/Flies'
+import Rivers from '../pages/Rivers'
 
 function Main(props) {
-
-    const URL = 'http://localhost:4000/api/river'
-    const [rivers, setRivers] = useState(null)  
-
-    // const getRiversRef = useRef();
-
-    async function getRivers() {
-        const response = await fetch(URL);
-        const data = await response.json();
-        setRivers(data)
-    }
-
-    useEffect(() => {
-        getRivers();
-    }, [])
-
     return (
         <main>
             <Switch>
 
-                <Route exact path="/">
-                    <Index rivers={rivers}/>
+                <Route exact path="/flies">
+                    <Flies/>
+                </Route>
+
+                <Route path="/rivers">
+                    <Rivers/>
                 </Route>
 
 

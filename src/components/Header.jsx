06a2b8cbd37dbc47939edf5dfd097845476cd932
jwsmith2340 <92739468+jwsmith2340@users.noreply.github.com>
2022-05-21 19:01:48 -0400

    Adding hooks to implement event handlers.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,25 @@
+import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { FaBars } from 'react-icons/fa'
 
 import '../public/styles/Header.scss'
 
 function Header(props) {
+
+    const [button, setButton] = useState('closed');
+
+    function changeButton() {
+        if (button === 'closed') {
+            setButton('open');
+        } else {
+            setButton('closed');
+        }
+    }
+
     return (
         <div className="navbar">
         <div className="mob-hamburger-box">
-            <h4 id="mob-header-hamburger-fa"><FaBars /></h4>
+            <h4 id="mob-header-hamburger-fa"><FaBars className={button} onClick={ changeButton }/></h4>
         </div>
     </div>
     );
